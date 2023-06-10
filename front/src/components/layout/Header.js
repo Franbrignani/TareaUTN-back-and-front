436e07ac20db4747e099058655060d6193340ff2
Franbrignani <brignanifran@gmail.com>
2023-06-10 10:33:03 -0300

    Corregi el header

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -3,8 +3,10 @@ import React from "react";
 const Header = (props) =>{
     return(
       <>
+      <header>
       <h1 id="titulo">Bookland<img className="libro" src="https://cdn-icons-png.flaticon.com/512/167/167756.png" alt="icon-libros"/>
       </h1>
+      </header>
       </>
     )
 }
